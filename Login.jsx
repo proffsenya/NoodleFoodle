a497fbe7b2e44eaa36675c688ac1d000d96df618
Pokemaster <zaurm2005@gmail.com>
2025-02-25 15:35:50 +0300

    Integration and navigation

--- Login.jsx
+++ Login.jsx
@@ -1,9 +1,16 @@
-const handleLogin = (e) => {
-  e.preventDefault();
-  if (email === testAccount.email && password === testAccount.password) {
-    setIsLoggedIn(true);
-    navigate("/profile");
-  } else {
-    setError("Invalid email or password");
-  }
-};
+return (
+  <div className="min-h-screen bg-white flex flex-col">
+    <Header />
+    <div className="flex-grow flex items-center my-20 pt-10 justify-center">
+      <LoginSection
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleLogin={handleLogin}
+        error={error}
+      />
+    </div>
+    <Footer />
+  </div>
+);
