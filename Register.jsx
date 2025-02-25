7b665bafd2865bada21fee33f25921ec2a91a6d2
Pokemaster <zaurm2005@gmail.com>
2025-02-25 22:20:47 +0300

    Adding a form state

--- Register.jsx
+++ Register.jsx
@@ -1,17 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import "../src/index.css";
-
-export default function Register() {
-  return (
-    <div className="min-h-screen bg-white flex flex-col">
-      <Header />
-      <div className="flex-grow flex items-center my-20 pt-10 justify-center bg-cover bg-center">
-        {/* RegisterSection will be added here */}
-      </div>
-      <Footer />
-    </div>
-  );
-}
+const [formData, setFormData] = useState({
+  fullName: "",
+  address: "",
+  cardNumber: "",
+  birthdate: "",
+  email: "",
+  password: "",
+});
