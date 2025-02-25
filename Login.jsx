433387ff426b1ca86fbaf67d5456b8c63113300c
Pokemaster <zaurm2005@gmail.com>
2025-02-25 15:29:35 +0300

    Adding a status

--- Login.jsx
+++ Login.jsx
@@ -1,19 +1,3 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import "../src/index.css";
-
-export default function Login({ setIsLoggedIn }) {
-  const navigate = useNavigate();
-
-  return (
-    <div className="min-h-screen bg-white flex flex-col">
-      <Header />
-      <div className="flex-grow flex items-center my-20 pt-10 justify-center">
-        {/* LoginSection will be added here */}
-      </div>
-      <Footer />
-    </div>
-  );
-}
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState("");
