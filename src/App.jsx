59ba60174bc503c23cb9939d3e67e102f703b6f3
Pokemaster <zaurm2005@gmail.com>
2025-02-25 23:02:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useState } from "react";
 
 import Contact from "../pages/Contact";
 import CustomProduct from "../pages/CustomProduct";
@@ -7,35 +8,123 @@ import Menu from "../pages/Menu";
 import NotFound from "../pages/NotFound";
 import ProductDetails from "../pages/ProductDetails";
 import ShoppingCart from "../pages/ShoppingCart";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import Register from "../pages/Register";
 
+import Header from "../components/Header";
 import "../src/index.css";
 
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
+  window.scrollTo(0, 0);
   return null;
 };
 
-
-const router = createBrowserRouter([
-  {index: true, element: <><Home /><ScrollToTop /></>},
-  {path: "contact", element: <><Contact /><ScrollToTop /></>},
-  {path: "customproduct", element: <><CustomProduct/><ScrollToTop /></>},
-  {path: "menu", element: <><Menu /><ScrollToTop /></>},
-  {path: "shoppingcart", element: <><ShoppingCart /><ScrollToTop /></>},
-  {path: "productdetails/:productid", element: <><ProductDetails /><ScrollToTop /></>},
-  {path: "*", element: <><NotFound /><ScrollToTop /></>},
-])
-
 export default function App() {
-  return (
-    <RouterProvider router = {router}/>
-  )
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <Home />
+          <ScrollToTop />
+        </>
+      ),
+    },
+    {
+      path: "/contact",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <Contact />
+          <ScrollToTop />
+        </>
+      ),
+    },
+    {
+      path: "/customproduct",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <CustomProduct />
+          <ScrollToTop />
+        </>
+      ),
+    },
+    {
+      path: "/menu",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <Menu />
+          <ScrollToTop />
+        </>
+      ),
+    },
+    {
+      path: "/shoppingcart",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <ShoppingCart />
+          <ScrollToTop />
+        </>
+      ),
+    },
+    {
+      path: "/productdetails/:productid",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <ProductDetails />
+          <ScrollToTop />
+        </>
+      ),
+    },
+    {
+      path: "/profile",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <Profile />
+          <ScrollToTop />
+        </>
+      ),
+    },
+    {
+      path: "/login",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <Login setIsLoggedIn={setIsLoggedIn} />
+          <ScrollToTop />
+        </>
+      ),
+    },
+    {
+      path: "/register",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <Register />
+          <ScrollToTop />
+        </>
+      ),
+    },
+    {
+      path: "*",
+      element: (
+        <>
+          <Header isLoggedIn={isLoggedIn} />
+          <NotFound />
+          <ScrollToTop />
+        </>
+      ),
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
